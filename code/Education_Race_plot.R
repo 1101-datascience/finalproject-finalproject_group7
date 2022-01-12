147ddaf008f8bff3fc8b79cf50412f0e5b55abc6
JohnLin0609 <80409356+JohnLin0609@users.noreply.github.com>
2022-01-12 20:45:55 +0800

    Add files via upload

--- code/Education_Race_plot.R
+++ code/Education_Race_plot.R
@@ -6,10 +6,17 @@ data <- read.csv("Levels_Fyi_Salary_Data.csv", encoding = "UTF-8")
 data <- na.omit(data)
 
 
-Education_plot <- ggplot(data, aes(x = basesalary, y = Education)) +
+Education_plot <- ggplot(data, aes(x = basesalary, y = company)) +
   geom_point()
 Education_plot
 
 Race_plot <- ggplot(data, aes(x = basesalary, y = Race)) +
   geom_point()
 Race_plot
+
+unique(data$title)
+
+ggplot(data, aes(x = as.factor(title), y = data.basesalary, color = as.factor(title))) +
+  geom_boxplot() +
+  xlab("title") +
+  ylab("basesalary")
